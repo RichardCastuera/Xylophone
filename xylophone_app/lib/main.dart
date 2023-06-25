77a7174fdbbad24e692d114692459dc3d8474929
Richard <richardcastuera16@gmail.com>
2023-06-25 16:00:16 +0800

    feat(xylophone_app): Imported English_Words Package

--- xylophone_app/lib/main.dart
+++ xylophone_app/lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(child: Text(nouns.first),),
         ),
       ),
     );
